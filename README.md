e551597af38cdc6963825b4c214b92225adf039e
Bruno Almeida <brunoalmeidasantos@hotmail.com.br>
2024-07-25 22:06:37 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,42 @@
+*Decodificador de Texto
+**Sobre
+Uma aplicação que criptografa mensagens digitalizadas e descreva mensagens criptografadas por ele.
+
+As "chaves" de criptografia utilizadas são:
+A letra "e" é convertida para "enter".
+A letra "i" é convertida para "imes."
+A letra "a" é convertida para "ai".
+A letra "o" é convertida para "ober".
+A letra "u" é convertida para "ufat".
+Requisitos:
+- Deve funcionar apenas com letras minúsculas.
+
+- Não devem ser utilizadas letras com acentos nem caracteres especiais.
+
+- Deve ser possível converter uma palavra para a versão criptografada e também retornar uma palavra criptografada para a versão original.
+
+Por exemplo:
+"gato" => "gaitober"
+
+gaitober" => "gato"
+
+A página deve ter campos para inserir o texto a ser criptografado ou descritivo, e a pessoa usuária deve poder escolher entre as duas opções O resultado deve ser exibido na tela.
+
+*Objetivo do projeto
+Atestar e solidificar o aprendizado adquirido durante o módulo de JavaScript, em manipulação de páginas web utilizando Java Script.
+
+Desenvolvido por *Buno Santos (git@github.com:brunoalmeidasantos/Desafio_Decodificador.git)
+
+*Tecnologias e recursos utilizados.
+HTML5
+Formulários.
+CSS
+Responsabilidade.
+Caixa flexível.
+Consultas de mídia.
+Estilização.
+JavaScript
+DOM.
+Funções.
+Definir tempo limite
+Estruturas de repetição.
---
+++ app.js
@@ -0,0 +1,89 @@
+let text;
+let textarea;
+
+function criptografar() {
+
+    const substitutions = {
+      'e': 'enter',
+      'i': 'imes',
+      'a': 'ai',
+      'o': 'ober',
+      'u': 'ufat'
+    };
+
+    text = document.getElementById("text").value; 
+    if (!validaLetras(text)){
+      return; 
+    }
+    // Função para substituir os caracteres de acordo com as regras especificadas
+    let criptografar = text.replace(/[eiaou]/g, match => substitutions[match]);
+
+    console.log("Texto Criptografado:", criptografar);
+    document.getElementById("botaoCopia").style.display = "block";
+    document.getElementById("imagem").style.display = "none";
+    document.getElementById("textoSaida").style.display = "block";
+    document.getElementById('textoSaida').value = criptografar;
+
+    return criptografar;
+  }
+  
+  function descriptografar() {
+
+    text = document.getElementById("text").value; 
+
+    // Função para substituir os caracteres de acordo com as regras especificadas (inversas)
+
+    descriptografar = substitute(text)
+    console.log("Texto Descriptografado:", descriptografar);
+   
+    document.getElementById("imagem").style.display = "none";
+    document.getElementById("textoSaida").style.display = "block";
+
+    document.getElementById('textoSaida').value = descriptografar;
+    return descriptografar;
+  }
+
+  function substitute(text) {
+    // Substituições especificadas (inversas)
+    const substitutions = {
+      'enter': 'e',
+      'imes': 'i',
+      'ai': 'a',
+      'ober': 'o',
+      'ufat': 'u'
+    };
+    return text.replace(/enter|imes|ai|ober|ufat/g, function(match) {
+      return substitutions[match];
+    });
+  }
+
+  function copyText() {
+    // Seleciona o texto da textarea
+    textarea = document.getElementById("textoSaida");
+    textarea.select();
+    textarea.setSelectionRange(0, 99999); // Para dispositivos móveis
+
+    // Copia o texto para a área de transferência
+    document.execCommand("copy");
+
+    // Alerta para o usuário que o texto foi copiado
+    alert("Seu texto foi copiado!");
+}
+
+document.getElementById("textoSaida").style.display = "none";
+document.getElementById("botaoCopia").style.display = "none";
+document.getElementById("imagem").style.display = "block";
+
+
+function validaLetras(input){ 
+        
+    // Procurar caracteres que não são letras minúsculas
+    let invalidChars = input.match(/[^a-z0-9.,;:!?'"()\- ]/g);
+  
+    // Se houver caracteres inválidos, gerar um alerta
+    if (invalidChars) {
+      alert('Apenas letras minúsculas são permitidas e sem acento ou pontuação.');
+      return false;     
+    }
+    return true;
+}
